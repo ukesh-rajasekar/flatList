32d28b4141a89f3af164bf34d139260d45daace8
Ukesh Rajasekar <z5288900@ad.unsw.edu.au>
2023-11-05 12:59:30 +1100

    feat: custom drawer updated

--- components/custom-drawer.tsx
+++ components/custom-drawer.tsx
@@ -1,70 +1,32 @@
-import React from 'react';
-import {
-   View,
-   Text,
-   ImageBackground,
-   Image,
-   TouchableOpacity,
-} from 'react-native';
-import tw from 'twrnc';
 import {
    DrawerContentScrollView,
    DrawerItemList,
 } from '@react-navigation/drawer';
-
+import React from 'react';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import tw from 'twrnc';
 
 const CustomDrawer = (props) => {
    return (
-      <View style={{ flex: 1 }}>
+      <View style={tw`flex-1`}>
          <DrawerContentScrollView
             {...props}
-            contentContainerStyle={tw`bg-slate-800`}
+            contentContainerStyle={tw`flex-1 bg-slate-800`}
          >
-            <ImageBackground
-               source={require('../assets/images/menu-bg.jpeg')}
-               style={{ padding: 20 }}
-            >
+            <View style={tw`flex flex-column items-center justify-center p-6`}>
                <Image
                   source={require('../assets/images/user-profile.jpg')}
-                  style={{
-                     height: 80,
-                     width: 80,
-                     borderRadius: 40,
-                     marginBottom: 10,
-                  }}
+                  style={tw`h-30 w-30 rounded-full mb-3 mt-5`}
                />
                {/* //change hello to user name if auth */}
-               <Text
-                  style={{
-                     color: '#fff',
-                     fontSize: 18,
-                     fontFamily: 'Roboto-Medium',
-                     marginBottom: 5,
-                  }}
-               >
-                  Hello!
-               </Text>
-               <View style={{ flexDirection: 'row' }}>
-                  <Text
-                     style={{
-                        color: '#fff',
-                        fontFamily: 'Roboto-Regular',
-                        marginRight: 5,
-                     }}
-                  >
-                     ....
-                  </Text>
+               <Text style={tw`text-lg text-slate-300`}>Hello!</Text>
+               <View style={tw`flex-row`}>
+                  <Text style={tw`text-lg text-slate-300`}>Log In</Text>
                </View>
-            </ImageBackground>
-            <View
-               style={{
-                  flex: 1,
-                  backgroundColor: '#fff',
-                  paddingTop: 10,
-                  width: '100%',
-               }}
-            >
+            </View>
+            <View style={tw`h-px my-2 w-full bg-gray-700 border-0`} />
+            <View style={tw`flex-1 bg-slate-800 py-5`}>
                <DrawerItemList {...props} />
             </View>
          </DrawerContentScrollView>
--- navigation/app-stack.tsx
+++ navigation/app-stack.tsx
@@ -2,6 +2,8 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import React from 'react';
 import CustomDrawer from '../components/custom-drawer';
 import MyList from '../screens/my-list';
+import Trash from '../screens/trash';
+import Appearance from '../screens/appearance';
 
 const Drawer = createDrawerNavigator();
 
@@ -11,15 +13,17 @@ export const AppStack = () => {
          drawerContent={(props) => <CustomDrawer {...props} />}
          screenOptions={{
             headerShown: true,
-            drawerActiveBackgroundColor: '#aa18ea',
-            drawerActiveTintColor: '#fff',
-            drawerInactiveTintColor: '#333',
+            drawerActiveBackgroundColor: '#334155',
+            drawerActiveTintColor: '#94A3B8',
+            drawerInactiveTintColor: '#94A3B8',
             drawerLabelStyle: {
-               fontSize: 15,
+               fontSize: 16,
             },
          }}
       >
          <Drawer.Screen name='My List' component={MyList} />
+         <Drawer.Screen name='Trash' component={Trash} />
+         <Drawer.Screen name='Appearance' component={Appearance} />
       </Drawer.Navigator>
    );
 };
